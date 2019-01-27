e9456d8f29758ee4b2749e5607a865a4212e78e3
Kelyn Crandall <kelyn@crandall.me>
2019-01-26 20:34:30 -0600

    Switched to using quad color for subdivision

--- mandelbrot.py
+++ mandelbrot.py
@@ -2,7 +2,7 @@ from numba import jit
 from math import sqrt, log
 
 @jit(cache = True)
-def render(zReal, zImag, maxIter = 1000):
+def render(zReal, zImag, maxIter = 100):
 	real = zReal
 	imag = zImag
 	for n in range(maxIter):
@@ -10,7 +10,7 @@ def render(zReal, zImag, maxIter = 1000):
 		real2 = real*real
 		imag2 = imag*imag
 		if real2 + imag2 > 4.0: # Check for divergence against 4 instead of 2 because of squared coordinates
-			return n # Return the number of iterations if this pixel diverges
+			return n/maxIter # Return the number of iterations if this pixel diverges
 		imag = 2 * real * imag + zImag # Finish the required math for this iteration if the pixel has not diverged yet
 		real = real2 - imag2 + zReal
 	return 0
--- renderer.py
+++ renderer.py
@@ -3,48 +3,62 @@ from scipy.misc import imsave
 import gradient, mandelbrot, cactus, julia
 import time
 from math import ceil, floor
+import itertools
 
 class Renderer():
-	def __init__(self, xResMax = 4096, yResMax = 4096, xResMin = 2, yResMin = 2):
-		self.xResMax = xResMax
-		self.yResMax = yResMax
-		self.xResMin = xResMin
-		self.yResMin = yResMin
+	def __init__(self, xRes = 512, yRes = 512):
+		self.xRes = xRes
+		self.yRes = yRes
 
-		self.cam = Camera(xResMax, yResMax, xPos = -.5)
+		self.cam = Camera(xRes, yRes, xPos = -.5)
 
-		weightImage = [[mandelbrot.renderDistEst(self.cam.convertX(x*2), self.cam.convertY(y*2)) for x in range(floor(self.xResMax/2))] for y in range(floor(self.yResMax/2))]
-		imsave('weightImage.png', weightImage)
+		imsave('dynamic.png', self.renderQuadImage(2000, 100))
+		imsave('fullRes.png', self.renderImage(100))
 
-		#imsave('dynamic.png', self.renderQuadImage(weightImage))
-
-		#imsave('fullRes.png', self.renderImage())
-
-	def renderImage(self):
+	def renderImage(self, maxIters):
 		t = time.clock()
 
-		# The list comprehension is faster, but less flexible
-		image = [[mandelbrot.render(self.cam.convertX(x), self.cam.convertY(y)) for x in range(self.xResMax)] for y in range(self.yResMax)]
-		#image = []
-		#for y in range(self.yResMax):
-		#	row = []
-		#	for x in range(self.xResMax):
-		#		row.append(mandelbrot.render(self.cam.convertX(x), self.cam.convertY(y)))
-		#	image.append(row)
+		image = [[mandelbrot.render(self.cam.convertX(x), self.cam.convertY(y), maxIters) for x in range(self.xRes)] for y in range(self.yRes)]
 
 		print("Render time was " + str(time.clock()-t) + " seconds.")
 		return image
 
-	def renderQuadImage(self, weightImage):
+	def renderQuadImage(self, subdivMax, maxIters):
+		def sparseRender(x, y, size):
+			corn = []
+			for i in [(x, y), (x+size, y), (x, y+size), (x+size, y+size)]:
+				if i in sparseArray:
+					corn.append(sparseArray[i])
+				else:
+					corn.append(mandelbrot.render(self.cam.convertX(i[0]), self.cam.convertY(i[1]), maxIters))
+					sparseArray[i] = corn[-1]
+			return(sum(corn)/4)
+
 		t = time.clock()
-		image = [[0 for x in range(self.xResMax)] for y in range(self.yResMax)]
-		quadSize = floor(self.xResMax/self.xResMin)
-		for y in range(self.yResMin):
-			for x in range(self.xResMin):
-				q = Quadtree((x*quadSize, y*quadSize), quadSize, weightImage)
-				q.render(image, self.cam)
+		image = [[0 for x in range(self.xRes)] for y in range(self.yRes)]
+		sparseArray = {}
+
+		s = floor(self.xRes/2)
+		quadList = [
+		Quad(0, 0, 1, sparseRender(0, 0, s)),
+		Quad(0, s, 1, sparseRender(0, s, s)),
+		Quad(s, 0, 1, sparseRender(s, 0, s)),
+		Quad(s, s, 1, sparseRender(s, s, s))]
+		for i in range(subdivMax):
+			quadList.sort(key = lambda q: q.priority)
+			if quadList[0].priority == 0:
+				break
+			current = quadList.pop(0)
+			newSize = floor(current.size/2)
+			for j in [(current.x, current.y), (current.x+newSize, current.y), (current.x, current.y+newSize), (current.x+newSize, current.y+newSize)]:
+				quadList.append(Quad(j[0], j[1], newSize, sparseRender(j[0], j[1], newSize)))
+		for q in quadList:
+			for y in range(q.y, q.y + SIZE_LIST[q.size]):
+				for x in range(q.x, q.x + SIZE_LIST[q.size]):
+					image[y][x] = q.x
 		print("Dynamic render time was " + str(time.clock()-t) + " seconds.")
-		return image
+		return(image)
+
 
 class Camera(): # This class is responsible for handling the conversion from pixel position to mathematical space
 	def __init__(self, xRes, yRes, xPos = 0, yPos = 0, zoom = 2):
@@ -63,52 +77,14 @@ class Camera(): # This class is responsible for handling the conversion from pix
 	def convertY(self, y):
 		return (y-self.yRes/2)*self.zoom/self.yRes-self.yPos
 
-class Quadtree():
-	def __init__(self, pos, size, weightImage):
-		self.children = []
-		self.pos = pos
+
+class Quad():
+	def __init__(self, x, y, size, color):
+		self.x = x
+		self.y = y
 		self.size = size
-		# Check if the weightImage asks for more subdivision
-		if size > 1:
-			for x in range(floor(self.pos[0]/2), floor((self.pos[0]+self.size)/2)):
-				for y in range(floor(self.pos[1]/2), floor((self.pos[1]+self.size)/2)):
-					if weightImage[y][x] > 0:
-						self.subdivide(weightImage)
-						return
+		self.color = color
+		self.priority = size*color
 
-	def render(self, image, cam):
-		if not self.children: # If this quad is not subdivided
-			half = floor(self.size/2)
-			#change this line to update the appropriate pixels of the image instead of returning
-			#return(gradient.render(self.pos[0]+half, self.pos[1]+half))
-			col = mandelbrot.render(cam.convertX(self.pos[0]+half), cam.convertY(self.pos[1]+half))
-			for x in range(self.pos[0], self.pos[0]+self.size):
-				for y in range(self.pos[1], self.pos[1]+self.size):
-					image[y][x] = col
-		else:
-			for c in self.children:
-				c.render(image, cam)
-
-	def subdivide(self, weightImage):
-		half = floor(self.size/2)
-		x, y = self.pos
-		self.children.append(Quadtree(self.pos, half, weightImage))
-		self.children.append(Quadtree((x+half, y), half, weightImage))
-		self.children.append(Quadtree((x, y+half), half, weightImage))
-		self.children.append(Quadtree((x+half, y+half), half, weightImage))
-
-class BoundingBox():
-	def __init__(self, coords, size):
-		self.coords = coords
-		self.size = size
-
-	def containsPoint(self, point):
-		if point[0] in range(self.coords[0], self.coords[0]+size):
-			if point[1] in range(self.coords[1], self.coords[1]+size):
-				return(True)
-		return(False)
-
-	def intersectsBB(self, otherBB):
-		return(False)
 
 Renderer()
