7628921eaa3e643e57decc33fd6b97fa370b2170
Kelyn Crandall <kelyn@crandall.me>
2019-01-29 23:13:37 -0600

    Update renderer.py
    Added Julia renderer classes.

--- renderer.py
+++ renderer.py
@@ -30,6 +30,30 @@ class FullRenderer():
 		print("Render time was " + str(time.clock()-t) + " seconds.")
 		return image
 
+
+class JuliaFullRenderer(FullRenderer):
+	def __init__(self, xRes = 512, yRes = 512, AA = 0, maxIters = 100, cx = 0.3, cy = .5):
+		super().__init__(xRes, yRes, AA, maxIters)
+		self.cx = cx
+		self.cy = cy
+
+	def render(self):
+		t = time.clock()
+
+		#image = [[mandelbrot.render(self.cam.convertX(x), self.cam.convertY(y), maxIters) for x in range(self.xRes)] for y in range(self.yRes)]
+		image = [[0 for x in range(self.xRes)] for y in range(self.yRes)]
+		for y in range(self.yRes):
+			for x in range(self.xRes):
+				pix = []
+				AAList = [(x+.25, y+.25), (x+.75, y+.75), (x+.25, y+.75), (x+.75, y+.25), (x+.5, y+.1), (x+.5, y+.9), (x+.1, y+.5), (x+.9, y+.5)]
+				for i in range(self.AA+1):
+					pix.append(julia.render(self.cam.convertX(AAList[i][0]), self.cam.convertY(AAList[i][1]), self.cx, self.cy, self.maxIters))
+				image[y][x] = sum(pix)
+
+		print("Render time was " + str(time.clock()-t) + " seconds.")
+		return image
+
+
 class QuadRenderer():
 	def __init__(self, res = 512, AA = 0, disableMaxResAA = True, subdivMax = 5000, maxIters = 100):
 		self.res = res
@@ -40,33 +64,33 @@ class QuadRenderer():
 
 		self.cam = Camera(res, res, xPos = -.5)
 
-	def render(self):
-		def sparseRender(x, y, size):
-			if size == 1 and self.disableMaxResAA:
-				return mandelbrot.render(self.cam.convertX(x), self.cam.convertY(y), self.maxIters)
-			half = size/2
-			pixelList = [(x, y), (x+size, y+size), (x+size, y), (x, y+size), (x+half, y+half), (x+half, y), (x, y+half), (x+half, y+size), (x+size, y+half)]
-			pix = []
-			for i in pixelList:
-				if len(pix) > self.AA+2:
-					break
-				if i in sparseArray:
-					pix.append(sparseArray[i])
-				else:
-					pix.append(mandelbrot.render(self.cam.convertX(i[0]), self.cam.convertY(i[1]), self.maxIters))
-					sparseArray[i] = pix[-1]
-			return sum(pix)
+	def sparseRender(self, x, y, size):
+		if size == 1 and self.disableMaxResAA:
+			return mandelbrot.render(self.cam.convertX(x), self.cam.convertY(y), self.maxIters)
+		half = size/2
+		pixelList = ((x, y), (x+size, y+size), (x+size, y), (x, y+size), (x+half, y+half), (x+half, y), (x, y+half), (x+half, y+size), (x+size, y+half))
+		pix = []
+		for i in pixelList:
+			if len(pix) > self.AA+2:
+				break
+			if i in self.sparseArray:
+				pix.append(self.sparseArray[i])
+			else:
+				pix.append(mandelbrot.render(self.cam.convertX(i[0]), self.cam.convertY(i[1]), self.maxIters))
+				self.sparseArray[i] = pix[-1]
+		return sum(pix)
 
+	def render(self):
 		t = time.clock()
 		image = [[0 for x in range(self.res)] for y in range(self.res)]
-		sparseArray = {}
+		self.sparseArray = {}
 
 		s = floor(self.res/2)
 		quadList = [
-		Quad(0, 0, s, sparseRender(0, 0, s)),
-		Quad(0, s, s, sparseRender(0, s, s)),
-		Quad(s, 0, s, sparseRender(s, 0, s)),
-		Quad(s, s, s, sparseRender(s, s, s))] # start with 4 quads that are 1/2 the image size on a side
+		Quad(0, 0, s, self.sparseRender(0, 0, s)),
+		Quad(0, s, s, self.sparseRender(0, s, s)),
+		Quad(s, 0, s, self.sparseRender(s, 0, s)),
+		Quad(s, s, s, self.sparseRender(s, s, s))] # start with 4 quads that are 1/2 the image size on a side
 		sortLimit = 0 # heuristically limit how often we sort to improve performance
 		subdivisions = 0
 		while subdivisions < self.subdivMax:
@@ -80,7 +104,7 @@ class QuadRenderer():
 			current = quadList.pop(0)
 			newSize = floor(current.size/2)
 			for j in [(current.x, current.y), (current.x+newSize, current.y), (current.x, current.y+newSize), (current.x+newSize, current.y+newSize)]:
-				quadList.append(Quad(j[0], j[1], newSize, sparseRender(j[0], j[1], newSize)))
+				quadList.append(Quad(j[0], j[1], newSize, self.sparseRender(j[0], j[1], newSize)))
 		for q in quadList:
 			for y in range(q.y, q.y + q.size):
 				for x in range(q.x, q.x + q.size):
@@ -90,10 +114,27 @@ class QuadRenderer():
 
 
 class JuliaQuadRenderer(QuadRenderer):
-	def __init__(self, res = 512, AA = 0, disableMaxResAA = True, subdivMax = 5000, maxIters = 100, cx = 0.1, cy = .25):
-		super.__init__(res, AA, disableMaxResAA, subdivMax, maxIters)
+	def __init__(self, res = 512, AA = 0, disableMaxResAA = True, subdivMax = 5000, maxIters = 100, cx = 0.3, cy = .5):
+		super().__init__(res, AA, disableMaxResAA, subdivMax, maxIters)
 		self.cx = cx
 		self.cy = cy
+
+	def sparseRender(self, x, y, size):
+		if size == 1 and self.disableMaxResAA:
+			return julia.render(self.cam.convertX(x), self.cam.convertY(y), self.cx, self.cy, self.maxIters)
+		half = size/2
+		pixelList = ((x, y), (x+size, y+size), (x+size, y), (x, y+size), (x+half, y+half), (x+half, y), (x, y+half), (x+half, y+size), (x+size, y+half))
+		pix = []
+		for i in pixelList:
+			if len(pix) > self.AA+2:
+				break
+			if i in self.sparseArray:
+				pix.append(self.sparseArray[i])
+			else:
+				pix.append(julia.render(self.cam.convertX(i[0]), self.cam.convertY(i[1]), self.cx, self.cy, self.maxIters))
+				self.sparseArray[i] = pix[-1]
+		return sum(pix)
+
 
 class RealtimeQuadRenderer():
 	def __init__(self, res = 512, AA = 0, disableMaxResAA = True, subdivMax = 5000, maxIters = 100):
@@ -189,7 +230,11 @@ class Quad():
 
 if __name__ == "__main__":
 	res = 512
+	quadR = QuadRenderer(res = res, AA = 4, disableMaxResAA = False, subdivMax = 15000)
 	fullR = FullRenderer(xRes = res, yRes = res, AA = 4)
-	quadR = QuadRenderer(res = res, AA = 2, disableMaxResAA = True, subdivMax = 15000)
+	juliaR = JuliaQuadRenderer(res = res, AA = 4, disableMaxResAA = False, subdivMax = 15000)
+	fullJuliaR = JuliaFullRenderer(xRes = res, yRes = res, AA = 4)
 	imsave('dynamic.png', quadR.render())
 	imsave('fullRes.png', fullR.render())
+	imsave('juliaQuad.png', juliaR.render())
+	imsave('julia.png', fullJuliaR.render())
