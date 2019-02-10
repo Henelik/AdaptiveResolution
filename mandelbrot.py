859f2e1705ff4b0af7f9dd1d68ae509277334688
Kelyn Crandall <kelyn@crandall.me>
2019-02-10 16:34:10 -0600

    Switched iterator to integer loop

--- mandelbrot.py
+++ mandelbrot.py
@@ -4,14 +4,17 @@ biggest = 255
 
 @jit(cache = True)
 def render(zReal, zImag, maxIter = 100):
+	iters = 0
 	real = zReal
 	imag = zImag
-	for n in range(maxIter):
+	while True:
+		iters += 1
 		# Square the real and imaginary components BEFORE checking for divergence
 		real2 = real*real
 		imag2 = imag*imag
 		if real2 + imag2 > 4.0: # Check for divergence against 4 instead of 2 because of squared coordinates
-			return n*biggest//maxIter # Return the number of iterations if this pixel diverges
+			return iters#*biggest//maxIter # Return the number of iterations if this pixel diverges
 		imag = 2 * real * imag + zImag # Finish the required math for this iteration if the pixel has not diverged yet
 		real = real2 - imag2 + zReal
-	return 0
+		if iters >= maxIter:
+			return 0
