dab0ff80828fa368cd49179ff929b2fe3e63af81
Kelyn (Desktop) <kelyn@crandall.me>
2019-03-10 19:11:20 -0500

    Added renderer widget to UI

--- application.py
+++ application.py
@@ -4,16 +4,18 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics.texture import Texture
 from kivy.clock import Clock
 from kivy.graphics import Rectangle
-from renderer import RealtimeQuadRenderer, RealtimeJuliaQuadRenderer, RealtimeCactusQuadRenderer
+from renderer import RealtimeQuadRenderer, RealtimeJuliaQuadRenderer, RealtimeCactusQuadRenderer, RealtimeGradientQuadRenderer
 
 class RendererWidget(Widget):
-	def __init__(self):
-		super().__init__()
+	def __init__(self, *args):
+		super().__init__(*args)
 		self.res = 512
-		self.texture = Texture.create(size=(self.res, self.res), bufferfmt="ubyte", colorfmt='rgb')
+		self.AA = 8
+		self.maxIters = 100
+		self.texture = Texture.create(size=(self.res, self.res), bufferfmt="ubyte", colorfmt="rgb")
 		with self.canvas:
 			Rectangle(texture=self.texture, pos=(0, 0), size=(self.res, self.res))
-		self.renderer = RealtimeCactusQuadRenderer(res = self.res, AA = 8, maxIters = 100)
+		self.renderer = RealtimeQuadRenderer(res = self.res, AA = self.AA, maxIters = self.maxIters)
 		self.renderer.begin()
 		Clock.schedule_interval(self.tick, 1 / 30.)
 
@@ -24,10 +26,26 @@ class RendererWidget(Widget):
 		self.texture.blit_buffer(self.renderer.image.tostring(), bufferfmt="ubyte", colorfmt="rgb")
 		self.canvas.ask_update()
 
+	def changeFractal(self, fractal):
+		if fractal == 'mandelbrot':
+			self.renderer = RealtimeQuadRenderer(res = self.res, AA = self.AA, maxIters = self.maxIters)
+		elif fractal == 'julia':
+			self.renderer = RealtimeJuliaQuadRenderer(res = self.res, AA = self.AA, maxIters = self.maxIters)
+		elif fractal == 'cactus':
+			self.renderer = RealtimeCactusQuadRenderer(res = self.res, AA = self.AA, maxIters = self.maxIters)
+		elif fractal == 'gradient':
+			self.renderer = RealtimeGradientQuadRenderer(res = self.res, AA = self.AA, maxIters = self.maxIters)
+		else:
+			raise(TypeError)
+			return
+		self.renderer.begin()
+
 
 class RootWidget(FloatLayout):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
+		self.renderer = RendererWidget()
+		self.add_widget(self.renderer)
 
 
 class RendererApp(App):
