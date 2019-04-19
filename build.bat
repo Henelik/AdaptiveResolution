56a1f1154d0ec50d9e9ed89e36d80df3abfa3bfd
Kelyn Crandall <kelyn@crandall.me>
2019-04-19 09:18:23 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 mkdir C:\Users\%USERNAME%\Documents\QuadtreeRendererDeploy
 cd C:\Users\%USERNAME%\Documents\QuadtreeRendererDeploy
 python -m PyInstaller --name quadrenderer C:\Users\%USERNAME%\Documents\QuadtreeRenderer\application.py
-copy C:\Users\%USERNAME%\Documents\QuadtreeRenderer\dist\quadrenderer\renderer.kv renderer.kv
+copy C:\Users\%USERNAME%\Documents\QuadtreeRenderer\dist\quadrenderer\quadrenderer.kv quadrenderer.kv
 robocopy C:\Users\%USERNAME%\Documents\QuadtreeRenderer\dist\quadrenderer\color_profiles\ \\color_profiles\
 PAUSE
